8238ddef12fc070ba80c5705b71c93dd8009b62d
Jules Cornelissen <git@nooby.eu>
2021-03-10 13:20:32 +0100

    Notification WIP

--- app/src/main/java/nl/tue/group2/Warranteed/MainActivity.java
+++ app/src/main/java/nl/tue/group2/Warranteed/MainActivity.java
@@ -1,21 +1,43 @@
 package nl.tue.group2.Warranteed;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Random;
+
+import nl.tue.group2.Warranteed.notifications.NotifInit;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel(getString(R.string.chan_general_app));
+
+        // Initialize notification channel for general notifications
+//        NotifInit notifInit = new NotifInit();
+//        notifInit.createNotificationChannel(getString(R.string.chan_general_app));
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,6 +47,38 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    private void createNotificationChannel(String CHANNEL_ID) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    Random r = new Random();
+
+    public void sendNotification(View view) {
+        int notifID = r.nextInt();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.chan_general_app))
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle("Some notifc" + notifID)
+                .setContentText("Hello World!");
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notifID, builder.build());
+
     }
 
 }
---
+++ app/src/main/java/nl/tue/group2/Warranteed/notifications/NotifInit.java
@@ -0,0 +1,28 @@
+package nl.tue.group2.Warranteed.notifications;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import nl.tue.group2.Warranteed.R;
+
+public class NotifInit extends AppCompatActivity {
+
+    public void createNotificationChannel(String CHANNEL_ID) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
