968ee479bc5a13b62d1332b4ea32770f56a84d79
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-03-16 20:27:31 +0100

    Validate the receipt data and add it to firebase from the 'upload receipt' screen

--- app/src/main/java/nl/tue/group2/Warranteed/ui/add/AddFragment.java
+++ app/src/main/java/nl/tue/group2/Warranteed/ui/add/AddFragment.java
@@ -10,14 +10,20 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import nl.tue.group2.Warranteed.R;
+import nl.tue.group2.Warranteed.ui.home.HomeFragment;
 
 public class AddFragment extends Fragment {
 
@@ -51,6 +57,7 @@ public class AddFragment extends Fragment {
                 v -> PhotoHandler.requestSelectPhoto(this)
         );
 
+        // purchase and expiration date fields
         this.purchaseDate = Calendar.getInstance();
         this.getActivity().findViewById(R.id.purchaseDateField).setOnClickListener(
                 v -> {
@@ -79,6 +86,19 @@ public class AddFragment extends Fragment {
                     dialog.show();
                 }
         );
+
+        // clear button
+        this.getActivity().findViewById(R.id.clearButton).setOnClickListener(
+                v -> ((EditText) this.getActivity().findViewById(R.id.itemField)).setText("")
+        );
+
+        // submit button
+        this.getActivity().findViewById(R.id.submitReceiptButton).setOnClickListener(
+                v -> {
+                    if (this.validateFields())
+                        this.completeReceipt();
+                }
+        );
     }
 
     @Override
@@ -104,5 +124,73 @@ public class AddFragment extends Fragment {
 
     private void setReceiptImage(Bitmap image) {
         this.receiptImage = image;
+    }
+
+    /**
+     * Checks that all fields have valid data, otherwise it displays an error
+     * @return true if all fields are valid
+     */
+    private boolean validateFields() {
+        boolean valid = true;
+
+        // name
+        EditText nameField = this.getActivity().findViewById(R.id.nameField);
+        if (nameField.getText().toString().trim().isEmpty()) {
+            nameField.setError("Field must not be empty!");
+            valid = false;
+        }
+        // purchase date
+        EditText purchaseDataField = this.getActivity().findViewById(R.id.purchaseDateField);
+        if (purchaseDataField.getText().toString().trim().isEmpty()) {
+            purchaseDataField.setError("Field must be set!");
+            valid = false;
+        }
+        // expiration date
+        EditText expirationDateField = this.getActivity().findViewById(R.id.expirationDateField);
+        if (expirationDateField.getText().toString().trim().isEmpty()) {
+            expirationDateField.setError("Field must be set!");
+            valid = false;
+        } else if (this.expirationDate.getTime().getTime() < this.purchaseDate.getTime().getTime()) {
+            expirationDateField.setError("Must be later than the purchase date!");
+            valid = false;
+        }
+        // item
+        EditText itemField = this.getActivity().findViewById(R.id.itemField);
+        if (itemField.getText().toString().trim().isEmpty()) {
+            itemField.setError("Field must not be empty!");
+            valid = false;
+        }
+        // image
+        if (this.receiptImage == null)
+            valid = false;
+
+        return valid;
+    }
+
+    /**
+     * Uploads the receipt to the server and switches the activity to the receipt's page
+     */
+    private void completeReceipt() {
+        String name = ((EditText) this.getActivity().findViewById(R.id.nameField)).getText().toString();
+        Date purchaseDate = this.purchaseDate.getTime();
+        Date expirationDate = this.expirationDate.getTime();
+        String item = ((EditText) this.getActivity().findViewById(R.id.itemField)).getText().toString();
+        Bitmap image = this.receiptImage;
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yy", Locale.US);
+
+        // put the data into a map
+        Map<String, String> data = new HashMap<>();
+        data.put("name", name);
+        data.put("purchase_date", dateFormatter.format(purchaseDate));
+        data.put("expiration_date", dateFormatter.format(expirationDate));
+        data.put("product", item);
+
+        // TODO send image data
+
+        // create a new receipt in firebase
+        FirebaseFirestore.getInstance().collection("Receipt").document().set(data).addOnCompleteListener(
+                task -> this.getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit()
+        );
     }
 }
